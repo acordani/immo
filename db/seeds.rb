272d932c7551c15a723807ae44e66a274e814a0a
Alexandre Cordani <alcordani@gmail.com>
2015-05-21 09:29:18 +0200

    ajout strong params dans announces_controller,seeds et modification formulaire pour y integrer property_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+puts "Deleting old records..."
+Property.destroy_all
+
+puts "Creating property..."
+Property.create(name: "Appartement")
+Property.create(name: "Maison")
+Property.create(name: "Local Commercial")
+
+puts "Done !"
