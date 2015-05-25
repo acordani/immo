649a7da3a7e10f890ae424182c6853fb1d6365c9
Alexandre Cordani <alcordani@gmail.com>
2015-05-25 23:25:02 +0200

    amelioration responsive du bouton de l'input de la home

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,15 @@ ActiveRecord::Schema.define(version: 20150525190441) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.string   "bed"
+    t.integer  "min_price"
+    t.integer  "max_price"
+    t.string   "locality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "uploads", force: :cascade do |t|
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
