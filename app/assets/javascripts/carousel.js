27a468f00db82650dabd28dc39b694428c480826
Alexandre Cordani <alcordani@gmail.com>
2015-06-12 17:49:13 +0200

    suite page show

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-      slideSpeed : 300,
-      singleItem:true
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
   });
 });
