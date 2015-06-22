66954dc70e0ab58a670399651b4b55ce1a8f73f1
Alexandre Cordani <alcordani@gmail.com>
2015-06-22 11:30:31 +0200

    ajout de where not dans similar_announce afin de retirer l'annonce de la show

--- app/controllers/announces_controller.rb
+++ app/controllers/announces_controller.rb
@@ -29,7 +29,7 @@ class AnnouncesController < ApplicationController
     @announce_coordinates = { lat: @announce.latitude, lng: @announce.longitude }
 
 
-    @similar_announces = Announce.where("locality ILIKE ?", "%#{@announce.locality}%").where("bed >= ?", "#{@announce.bed}")
+    @similar_announces = Announce.where("locality ILIKE ?", "%#{@announce.locality}%").where("bed >= ?", "#{@announce.bed}").where.not(id: @post)
 
     # @similar_announces = Announce.where("bed >= ?", "#{@announce.bed}")
 
